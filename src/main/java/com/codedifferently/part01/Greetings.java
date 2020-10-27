7e66b11f36b65a10ceeee62161c882f4d7daa847
c-wspoon1 <corban.wspoon@gmail.com>
2020-10-27 22:54:22 +0000

    myFirstCommit

--- src/main/java/com/codedifferently/part01/Greetings.java
+++ src/main/java/com/codedifferently/part01/Greetings.java
@@ -1,7 +1,42 @@
 package com.codedifferently.part01;
 
-public class Greetings {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-    }
+public class Greetings {
+  public static void main(String[] args) {
+        
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("What's your name? ");
+    String input = scanner.next();
+   // System.out.println("Your name is " + input);
+   // TODO If the user's name is not "Hakim" or "Rasheeda" say "I dont know you cuz, what hood you stay, where yo grandma, where yo familiy say??"
+    
+    if (input.equalsIgnoreCase("Hakim") || input.equalsIgnoreCase("rasheeda")){
+    System.out.printf("Hello, %s!\n", input);
+   } else {
+       System.out.print("I'm gon' ask you one more time, homie. Where is you from? Or it is a problem.");
+   }
+   
+        
+  }  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+         // Console console = System.console();
+        // String string = "What's your name? ";
+        // String input = console.readLine(string);
+        // console.printf("Hello %s!%n ", input);
+
+  //  }  
